8f1a96c6d976f5837bfa9234c30f327bd8e521f2
Jens Nikolaus <jensnikolaus@gmail.com>
2023-01-11 20:49:15 -0700

    Wrap up day 14 learning about optionals

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,7 @@
 import Cocoa
 
 // Day 14
+// Unwrapping optionals
 
 let opposites = [
     "Mario": "Wario",
@@ -32,3 +33,69 @@ if let number = number {
 } else {
     print("No, number here")
 }
+
+// Guard
+func printSquare(of number: Int?) {
+    guard let number = number else {
+        print("Missing input")
+        return
+    }
+    
+    print("\(number) x \(number) is \(number * number)")
+}
+
+printSquare(of: 4)
+
+let fruit = [
+    "apple": "red",
+    "orange": "orange",
+    "lemon": "yellow"
+]
+
+let yummyFruit = fruit["lime"] ?? "No fruit available."
+
+let lightsaberColor: String? = "green"
+let color = lightsaberColor ?? "blue"
+
+// Optional chaining
+let names = ["Arya", "Bran", "Robb", "Sansa"]
+let chosen = names.randomElement()?.uppercased() ?? "No one"
+print("Next in line: \(chosen)")
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+var book: Book? = nil
+let author = book?.author?.first?.uppercased() ?? "A"
+print(author)
+
+let capitals = ["Scotland": "Edinburgh", "Wales": "Cardiff"]
+let scottishCapital = capitals["Scotland"]?.uppercased()
+
+// Function failure with optionals
+enum UserError: Error {
+    case badID, networkFailed
+}
+
+func getUser(id: Int) throws -> String {
+    throw UserError.networkFailed
+}
+
+if let user = try? getUser(id: 23) {
+    print("User: \(user)")
+}
+
+let user = (try? getUser(id: 23)) ?? "Anonymous"
+print(user)
+
+// Checkpoint 9
+// Function that accepts an optional array of integers, and returns one of those integers
+// If the array is missing or empty, return a new random number in the range of 1 through 100
+// Write your function in a single line of code
+
+func returnNumber(_ numbers: [Int]) -> Int { return numbers.randomElement() ?? Int.random(in: 1...100) }
+
+returnNumber([])
+returnNumber([5,9,19,39,105])
