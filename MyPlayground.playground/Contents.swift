ce7c40b64bfbceb9c2ccc7f570942d477c991409
Jens Nikolaus <jensnikolaus@gmail.com>
2023-01-11 19:17:12 -0700

    start day 14

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,194 +1,34 @@
 import Cocoa
 
-// Day 13
+// Day 14
 
-// Blueprint
-protocol Vehicle {
-    var name: String { get }
-    var currentPassengers: Int { get set }
-    
-    func estimateTime(for distance: Int) -> Int
-    func travel(distance: Int)
+let opposites = [
+    "Mario": "Wario",
+    "Luigi": "Waluigi"
+]
+
+if let marioOpposite = opposites["Mario"] {
+    print("Mario's opposite is \(marioOpposite)")
 }
 
-struct Car: Vehicle {
-    let name = "Car"
-    var currentPassengers = 1
-    
-    func travel(distance: Int) {
-        print("I am driving \(distance)km.")
-    }
-    
-    func estimateTime(for distance: Int) -> Int {
-        distance / 50
-    }
-    
-    func openSunroof() {
-        print("It's a nice day.")
-    }
+var username: String? = nil
+
+if let unwrappedName = username {
+    print("We got a user: \(unwrappedName)")
+} else {
+    print("The optional was empty.")
 }
 
-struct Bike: Vehicle {
-    let name = "Bike"
-    var currentPassengers = 1
-    
-    func travel(distance: Int) {
-        print("I am cycling \(distance) km.")
-    }
-    
-    func estimateTime(for distance: Int) -> Int {
-        distance / 10
-    }
+func square(number: Int) -> Int {
+    number * number
 }
 
-func commute(distance: Int, using vehicle: Vehicle) {
-    if vehicle.estimateTime(for: distance) > 100 {
-        print("That's too slow!")
-    } else {
-        vehicle.travel(distance: distance)
-    }
+var number: Int? = nil
+//number = 12
+
+// Temporarily creating a second constant with the same name, it is only available inside condition body, shadowing
+if let number = number {
+    square(number: number)
+} else {
+    print("No, number here")
 }
-
-func getTravelEstimates(using vehicles: [Vehicle], distance: Int) {
-    for vehicle in vehicles {
-        let estimate = vehicle.estimateTime(for: distance)
-        print ("\(vehicle.name): \(estimate) hours to travel \(distance)km.")
-    }
-}
-
-let car = Car()
-commute(distance: 100, using: car)
-
-let bike = Bike()
-commute(distance: 50, using: bike)
-
-getTravelEstimates(using: [car, bike], distance: 150)
-
-// Opaque return types
-func getRandomNumber() -> some Equatable {
-    Int.random(in: 1...6)
-}
-
-func getRandomBool() -> some Equatable {
-    Bool.random()
-}
-
-print(getRandomNumber() == getRandomNumber())
-
-// Extensions
-var quote = "   I like chocolate  ."
-let trimmed = quote.trimmingCharacters(in: .whitespacesAndNewlines)
-print(trimmed)
-
-extension String {
-    // Returning a value: -ed
-    func trimmed() -> String {
-        self.trimmingCharacters(in: .whitespacesAndNewlines)
-    }
-    
-    // Change current value, eg. sort()
-    mutating func trim() {
-        self = self.trimmed()
-    }
-    
-    var lines: [String] {
-        self.components(separatedBy: .newlines)
-    }
-}
-
-quote.trimmed()
-quote.trim()
-
-let lyrics = """
-Whitespace is the general term of the space character, the tab character, and a variety of other variants on those two. New lines are line breaks in text, which might sound simple but in practice of course there is no single one way of making them, so when we ask to trim new lines it will automatically take care of all the variants for us.
-
-For example, here’s a string that has whitespace on either side:
-"""
-print(lyrics.lines.count)
-
-struct Book {
-    let title: String
-    let pageCount: Int
-    let readingHours: Int
-}
-
-extension Book {
-    init(title: String, pageCount: Int) {
-        self.title = title
-        self.pageCount = pageCount
-        self.readingHours = pageCount / 50
-    }
-}
-
-let myBookTitle = Book(title: "Title", pageCount: 259, readingHours: 8)
-
-// Protocol extensions, protocol oriented programming
-extension Collection {
-    var isNotEmpty: Bool {
-        isEmpty == false
-    }
-}
-
-let guests = ["Mario", "Luigi", "Daisy"]
-
-if guests.isNotEmpty {
-    print("Guest count: \(guests.count)")
-}
-
-protocol Person {
-    var name: String { get }
-    func sayHello()
-}
-
-extension Person {
-    func sayHello() {
-        print("Hi, I am \(name)!")
-    }
-}
-
-struct Employee: Person {
-    let name: String
-}
-
-let taylor = Employee(name: "Taylor Swift")
-taylor.sayHello()
-
-// Checkpoint 8
-// Create protocol Building
-// Add rooms: Int, cost: Int, agentName: String
-// Method that prints summary of the agent selling the building
-// Two structs: House and Office
-
-protocol Building {
-    var rooms: Int { get }
-    var cost: Int { get }
-    var agentName: String { get }
-    
-    func printSummary()
-}
-
-struct House: Building {
-    var rooms: Int
-    var cost: Int
-    var agentName: String
-    
-    func printSummary() {
-        print("The house has got \(rooms) rooms and costs $\(cost). Call \(agentName) to set up a tour.")
-    }
-}
-
-struct Office: Building {
-    var rooms: Int
-    var cost: Int
-    var agentName: String
-    
-    func printSummary() {
-        print("The office has got \(rooms) rooms and costs $\(cost). Call \(agentName) to set up a tour.")
-    }
-}
-
-let house = House(rooms: 4, cost: 250_000, agentName: "Roger Rabit")
-house.printSummary()
-
-let office = Office(rooms: 2, cost: 320_000, agentName: "Dori")
-office.printSummary()
