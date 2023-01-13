9be802ff684dd228b5282e2ff5d328e2a6981d1f
Jens Nikolaus <jensnikolaus@gmail.com>
2023-01-13 12:28:04 -0700

    Add notes reviewing everything we learned so far on day 15

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,40 +1,150 @@
 import Cocoa
 
-// Day 14
-// Unwrapping optionals
+// Day 15: Reviewing everything learned so far
 
-let opposites = [
-    "Mario": "Wario",
-    "Luigi": "Waluigi"
+let quote = "You cannot shake hands with a clenched fist."
+let author = "Indira Gandhi"
+
+// String interpolation
+print("\(quote) – \(author)")
+
+print(quote.hasPrefix("You"))
+print(author.hasSuffix("gandhi"))
+
+let id = Int.random(in: 1...1000)
+print(id)
+
+let dog: [String: Any] = [
+    "name": "Sophie",
+    "cuteness": 11
 ]
 
-if let marioOpposite = opposites["Mario"] {
-    print("Mario's opposite is \(marioOpposite)")
+print(dog["cuteness", default: "Too cute!"])
+
+enum Weekday {
+    case monday, tuesday, wednesday, thursday, friday
 }
 
-var username: String? = nil
+let dayOff = Weekday.friday
 
-if let unwrappedName = username {
-    print("We got a user: \(unwrappedName)")
-} else {
-    print("The optional was empty.")
+switch dayOff {
+case .friday:
+    print("Let's start the weekend! ☀️")
+default:
+    print("It's a work day!")
 }
 
-func square(number: Int) -> Int {
-    number * number
+var day = Weekday.monday
+day = .friday
+
+var albums: [String] = ["Red", "Fearless"]
+
+// New array of strings
+var clues = [String]()
+
+let age = 13
+let canVote = age >= 18 ? "Yes" : "Sorry, you can't vote"
+print(canVote)
+
+func greet(_ person: String, formal: Bool = false) {
+    if formal {
+        print("Welcome, \(person)")
+    } else {
+        print("Hiiiii!")
+    }
 }
 
-var number: Int? = nil
-//number = 12
+greet("Jane")
 
-// Temporarily creating a second constant with the same name, it is only available inside condition body, shadowing
-if let number = number {
-    square(number: number)
-} else {
-    print("No, number here")
+// Closures
+let team = ["Gloria", "Suzan", "Tiffany", "Tasha"]
+
+//let onlyT = team.filter({ (name: String) -> Bool in
+let onlyT = team.filter { name in
+    name.hasPrefix("T")
 }
 
-// Guard
+// Alternative: let onlyT = team.filter { $0.hasPrefix("T") }
+
+print(onlyT)
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Set score to \(score)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+game.score += 10
+
+struct BankAccount {
+    private(set) var funds = 0
+    
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+    
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds > amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+let account = BankAccount(funds: 100)
+print(account.funds)
+//account.funds += 1000 Not allowed due to setting var to private
+
+class Employee {
+    let hours: Int
+    
+    init(hours: Int) {
+        self.hours = hours
+    }
+    
+    func printSummary() {
+        print("I work \(hours) hours a day.")
+    }
+}
+
+class Developers: Employee {
+    func work() {
+        print("I am coding for \(hours) hours a day.")
+    }
+    
+    override func printSummary() {
+        print("I spent hours and hours a day coding Swift.")
+    }
+}
+
+let andrea = Developers(hours: 8)
+andrea.work()
+andrea.printSummary()
+
+class Site {
+    let id: Int
+    
+    init(id: Int) {
+        self.id = id
+        print("Site \(id): Created")
+    }
+    
+    deinit {
+        print("Site \(id): Destroyed")
+    }
+}
+
+for i in 1...3 {
+    let site = Site(id: i)
+    print("Site \(site.id): I am in control!")
+}
+
 func printSquare(of number: Int?) {
     guard let number = number else {
         print("Missing input")
@@ -44,58 +154,7 @@ func printSquare(of number: Int?) {
     print("\(number) x \(number) is \(number * number)")
 }
 
-printSquare(of: 4)
-
-let fruit = [
-    "apple": "red",
-    "orange": "orange",
-    "lemon": "yellow"
-]
-
-let yummyFruit = fruit["lime"] ?? "No fruit available."
-
-let lightsaberColor: String? = "green"
-let color = lightsaberColor ?? "blue"
-
-// Optional chaining
-let names = ["Arya", "Bran", "Robb", "Sansa"]
-let chosen = names.randomElement()?.uppercased() ?? "No one"
-print("Next in line: \(chosen)")
-
-struct Book {
-    let title: String
-    let author: String?
-}
-
-var book: Book? = nil
-let author = book?.author?.first?.uppercased() ?? "A"
-print(author)
-
-let capitals = ["Scotland": "Edinburgh", "Wales": "Cardiff"]
-let scottishCapital = capitals["Scotland"]?.uppercased()
-
-// Function failure with optionals
-enum UserError: Error {
-    case badID, networkFailed
-}
-
-func getUser(id: Int) throws -> String {
-    throw UserError.networkFailed
-}
-
-if let user = try? getUser(id: 23) {
-    print("User: \(user)")
-}
-
-let user = (try? getUser(id: 23)) ?? "Anonymous"
-print(user)
-
-// Checkpoint 9
-// Function that accepts an optional array of integers, and returns one of those integers
-// If the array is missing or empty, return a new random number in the range of 1 through 100
-// Write your function in a single line of code
-
-func returnNumber(_ numbers: [Int]) -> Int { return numbers.randomElement() ?? Int.random(in: 1...100) }
-
-returnNumber([])
-returnNumber([5,9,19,39,105])
+let names = ["Sophie", "Gaius", "Stellah"]
+//let names = [String]()
+let chosen = names.randomElement()?.uppercased()
+print("Next in line: \(chosen ?? "No one")")
