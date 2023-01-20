992473611811f6200cf4fff4b39e67406ee841f4
Jens Nikolaus <jensnikolaus@gmail.com>
2023-01-20 11:52:09 -0700

    revise design to be a little nicer

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -17,28 +17,54 @@ struct ContentView: View {
     
     var body: some View {
         ZStack() {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            
                 .ignoresSafeArea()
-            VStack(spacing: 30) {
-                VStack() {
-                    Text("Tap the flag of")
+            
+            VStack() {
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
                         .foregroundColor(.white)
-                        .font(.subheadline.weight(.heavy))
-                    Text(countries[correctAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
                 
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .shadow(radius: 8)
+                Spacer()
+                
+                VStack(spacing: 15) {
+                    VStack() {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 8)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 60)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .padding()
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Spacer()
             }
         }
         .alert(scoreTitle, isPresented: $showingScore) {
@@ -47,6 +73,7 @@ struct ContentView: View {
             Text("Your score is \(score)")
         }
     }
+    
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
