72e34b7875a9fed28aab99b5016580095ab9df8b
Jens Nikolaus <jensnikolaus@gmail.com>
2023-01-25 19:12:29 -0700

    add game logic

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -16,65 +16,164 @@
 import SwiftUI
 
 struct ContentView: View {
-    let shapes = ["🪨", "📄", "✂️"]
-    var score = 0
+    @State private var score = 0
+    @State private var round = 0
     
-    @State private var currentShape = "✂️"
+    let rock = "🪨"
+    let paper = "📄"
+    let scissors = "✂️"
+    
+    @State private var shapes = ["🪨", "📄", "✂️"].shuffled()
+    @State private var currentShape = Int.random(in: 0...2)
     @State private var winOrLose = Bool.random()
+    
+    @State private var showingHighscore = false
     
     var body: some View {
         NavigationView {
             VStack {
-                Text("Rock, paper, scissors")
-                Text(winOrLose ? "Win" : "Lose")
-                    .font(.title)
-                    .frame(height: 40)
                 
                 Spacer()
                 
                 VStack {
-                    Text("\(shapes[0])")
+                    Text("\(shapes[currentShape])")
                         .font(.system(size: 200))
                         .shadow(radius: 40)
                 }
                 
                 Spacer()
                 
+                Text(winOrLose ? "Win the game" : "Lose the game")
+                    .font(.title)
+                    .frame(height: 40)
+                
                 HStack(spacing: 20) {
                     Button {
-                        
+                        choiceTapped(rock)
                     } label: {
-                        Text("🪨")
+                        Text(rock)
                             .frame(maxWidth: .infinity, minHeight: 40)
                     }
                     .buttonStyle(.bordered)
                     Button {
-                        
+                        choiceTapped(paper)
                     } label: {
-                        Text("📄")
+                        Text(paper)
                             .frame(maxWidth: .infinity, minHeight: 40)
                     }
                     .buttonStyle(.bordered)
                     Button {
-                        
+                        choiceTapped(scissors)
                     } label: {
-                        Text("✂️")
+                        Text(scissors)
                             .frame(maxWidth: .infinity, minHeight: 40)
                     }
                     .buttonStyle(.bordered)
                 }
                 .padding(20)
                 
-                
                 Spacer()
                 
                 VStack {
-                    Text("Player score: \(score)")
+                    Text("Score: \(score)")
                         .font(.headline)
                 }
             }
+            .navigationTitle("Rock, paper, scissors")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .alert("Congratulations!", isPresented: $showingHighscore) {
+            Button("Play again", action: resetGame)
+        } message: {
+            Text("Your score: \(score)")
         }
     }
+    
+    func shuffleShapes() {
+        shapes.shuffle()
+        currentShape = Int.random(in: 0...2)
+    }
+    
+    func choiceTapped(_ choice: String) {
+        if winOrLose == true {
+            if shapes[currentShape].contains(rock) {
+                if choice == rock {
+                    score -= 1
+                } else if choice == paper {
+                    score += 1
+                } else if choice == scissors {
+                    score -= 1
+                }
+            }
+            
+            if shapes[currentShape].contains(paper) {
+                if choice == rock {
+                    score -= 1
+                } else if choice == paper {
+                    score -= 1
+                } else if choice == scissors {
+                    score += 1
+                }
+            }
+            
+            if shapes[currentShape].contains(scissors) {
+                if choice == rock {
+                    score += 1
+                } else if choice == paper {
+                    score -= 1
+                } else if choice == scissors {
+                    score -= 1
+                }
+            }
+        } else if winOrLose == false {
+            if shapes[currentShape].contains(rock) {
+                if choice == rock {
+                    score += 1
+                } else if choice == paper {
+                    score -= 1
+                } else if choice == scissors {
+                    score += 1
+                }
+            }
+            
+            if shapes[currentShape].contains(paper) {
+                if choice == rock {
+                    score += 1
+                } else if choice == paper {
+                    score += 1
+                } else if choice == scissors {
+                    score -= 1
+                }
+            }
+            
+            if shapes[currentShape].contains(scissors) {
+                if choice == rock {
+                    score -= 1
+                } else if choice == paper {
+                    score += 1
+                } else if choice == scissors {
+                    score += 1
+                }
+            }
+        }
+        
+        shuffleShapes()
+        winOrLose.toggle()
+        
+        round += 1
+        if round == 10 {
+            showingHighscore = true
+        }
+    }
+    
+    func resetGame() {
+        round = 0
+        score = 0
+        
+        shuffleShapes()
+        winOrLose.toggle()
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
