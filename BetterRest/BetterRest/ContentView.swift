ff99bd891f075e56f98e8517c36b14320f702a73
Jens Nikolaus <jensnikolaus@gmail.com>
2023-01-27 12:02:23 -0700

    learn how to work with dates

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -10,9 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Hello, world!")
+//            Text(Date.now, format: .dateTime.hour().minute().month().day())
+            Text(Date.now.formatted(date: .long, time: .shortened))
         }
         .padding()
+    }
+    
+    func trivialExample() {
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        let date = Calendar.current.date(from: components) ?? Date.now
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
     }
 }
 
