60dd81220ec9e7ab85249b56c0f8b7a056274ed0
Jens Nikolaus <jensnikolaus@gmail.com>
2023-01-29 13:21:17 -0700

    add some test functions to learn about working with strings

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -22,8 +22,38 @@ struct ContentView: View {
             .listStyle(.grouped)
             .navigationTitle("WordScramble")
         }
-        
     }
+    
+    func test() {
+        let input = """
+a
+b
+c
+"""
+        let letters = input.components(separatedBy: "\n")
+        let letter = letters.randomElement()
+        
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    func test2() {
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location == NSNotFound
+    }
+    
+//    func loadFile() {
+//        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
+//            if let fileContents = try? String(contentsOf: fileURL) {
+//                // we loaded the file into the string
+//                fileContents
+//            }
+//        }
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
