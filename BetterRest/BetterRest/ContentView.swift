7767a13f58b419b58a4908cc1fe6dcb67385055b
Jens Nikolaus <jensnikolaus@gmail.com>
2023-01-28 19:08:38 -0700

    replace Stepper with Picker component

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -41,7 +41,11 @@ struct ContentView: View {
                 }
                 
                 Section {
-                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                    Picker("Cups of coffee", selection: $coffeeAmount) {
+                        ForEach(0...20, id: \.self) {
+                            Text($0, format: .number)
+                        }
+                    }
                 } header: {
                     Text("Daily coffee intake")
                 }
