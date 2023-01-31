2de872a9b4461112f7a2e10cfeec2edf77410a23
Jens Nikolaus <jensnikolaus@gmail.com>
2023-01-31 15:55:59 -0700

    start WordScramble app code

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,10 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
     var body: some View {
-        VStack {
-            Text("I love my dog")
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                } header: {
+                    Text("Used words")
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
         }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        // extra validation to come
+        
+        withAnimation() {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
     }
 }
 
