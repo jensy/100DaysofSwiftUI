d2c884e8eb8138af68fbe76ac5dba682322a1e61
Jens Nikolaus <jensnikolaus@gmail.com>
2023-02-04 11:56:08 -0700

    add custom .pivot animation using ViewModifier

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,31 +7,47 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    let letters = Array("Hello, SwiftUI")
-    @State private var enabled = false
-    @State private var dragAmount = CGSize.zero
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
 
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+            )
+    }
+}
+
+struct ContentView: View {
+    @State private var isShowingRed = false
+    
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count, id: \.self) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .background(enabled ? .blue : .red)
-                    .offset(dragAmount)
-                    .animation(
-                        .default.delay(Double(num) / 20),
-                        value: dragAmount)
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                .onEnded { _ in
-                    dragAmount = .zero
-                    enabled.toggle()
-                }
-        )
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
     }
 }
 
