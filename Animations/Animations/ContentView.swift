e12244009ccbb6b52b4a807589b86829f006d55b
Jens Nikolaus <jensnikolaus@gmail.com>
2023-02-04 11:43:22 -0700

    add 'Hello, SwiftUI ' animation

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,22 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("Hello, SwiftUI")
+    @State private var enabled = false
     @State private var dragAmount = CGSize.zero
 
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [.yellow, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-            .offset(dragAmount)
-            .gesture(
-                DragGesture()
-                    .onChanged { dragAmount = $0.translation }
-                    .onEnded { _ in
-                        withAnimation {
-                            dragAmount = .zero
-                        }
-                    }
-            )
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(
+                        .default.delay(Double(num) / 20),
+                        value: dragAmount)
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
     }
 }
 
