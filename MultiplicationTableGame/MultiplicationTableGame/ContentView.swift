bae47bdeb179b0878f1aee1baf988814b1c61bb7
Jens Nikolaus <jensnikolaus@gmail.com>
2023-02-09 09:38:27 -0700

    add settings and game mode views that and animate in/out depending on game state

--- MultiplicationTableGame/MultiplicationTableGame/ContentView.swift
+++ MultiplicationTableGame/MultiplicationTableGame/ContentView.swift
@@ -15,44 +15,91 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var gameMode = false
+    
     @State private var multiplicationTable = 2
     @State private var questions = [5, 10, 20]
+    
+    @State private var answer = 35
     
     var body: some View {
         NavigationView {
             VStack {
-                Stepper(value: $multiplicationTable, in: 2...12, step: 1) {
-                    Text("Multiplication table: \(multiplicationTable)")
-                }
                 
-                Section {
-                    Picker("How many questions?", selection: $questions) {
-                        ForEach(0..<questions.count, id: \.self) {
-                            Text("\(self.questions[$0])")
+                VStack {
+                    Stepper(value: $multiplicationTable, in: 2...12, step: 1) {
+                        Text("Multiplication table: \(multiplicationTable)")
+                    }
+                    
+                    Section {
+                        Picker("How many questions?", selection: $questions) {
+                            ForEach(0..<questions.count, id: \.self) {
+                                Text("\(self.questions[$0])")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    } header: {
+                        HStack {
+                            Text("How many questions?")
+                            Spacer()
                         }
                     }
-                    .pickerStyle(.segmented)
-                } header: {
-                    HStack {
-                        Text("How many questions?")
-                        Spacer()
-                    }
-                }
-                
-                Button {
                     
-                } label: {
-                    Text("Start game")
-                    .frame(maxWidth: .infinity)
+                    Button {
+                        withAnimation(
+                            .easeInOut(duration: 0.8)
+                            .delay(0.6)
+                        ) {
+                            startGame()
+                        }
+                    } label: {
+                        Text("Start multiplying")
+                            .frame(maxWidth: .infinity)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.primary)
                 }
-                .buttonStyle(.borderedProminent)
+                .frame(maxHeight: gameMode ? 0 : .infinity)
+                .opacity(gameMode ? 0 : 1)
                 
                 Spacer()
                 
+                //  Game mode on
+                VStack {
+                    Form {
+                        Text("What is 7 x 5?")
+                        
+                        Section("Your answer") {
+                            TextField("Your answer", value: $answer, format: .number)
+                        }
+                        
+                        Button {
+                            newQuestion()
+                        } label: {
+                            Text("Start multiplying")
+                                .frame(maxWidth: .infinity)
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .tint(.primary)
+                    }
+                }
+                .opacity(gameMode ? 1 : 0)
             }
             .padding(20)
             .navigationTitle("Mutiply Game")
         }
+    }
+    
+    func startGame() {
+        //  Start game switches on gameMode
+        gameMode = true
+        
+        //  Game mode will hide settings
+        //  Game mode will show form: Question, answer, next button
+    }
+    
+    func newQuestion() {
+        //  Ask next question
     }
 }
 
