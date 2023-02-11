f909de20d87de49102788b6dc8f3e1084b47c37d
Jens Nikolaus <jensnikolaus@gmail.com>
2023-02-11 16:21:43 -0700

    add a list with onDelete() and EditButton() functionality

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,33 +7,35 @@
 
 import SwiftUI
 
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    
-    let name: String
-    
-    var body: some View {
-        Text("Hello, \(name)")
-        Button("Dismiss", role: .destructive) {
-            dismiss()
-        }
-        .buttonStyle(.borderedProminent)
-
-    }
-}
-
 struct ContentView: View {
-    @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-        VStack {
-            Button("Show Sophie Noodle") {
-                showingSheet.toggle()
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                .listStyle(.grouped)
+                
+                Button("Add number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .navigationTitle("onDelete()")
+            .toolbar {
+                EditButton()
             }
         }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "Sophie, woof")
-        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
