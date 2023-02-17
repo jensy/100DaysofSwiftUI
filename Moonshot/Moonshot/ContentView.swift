96ef1b0cb72e25ca517490a57b8d00eaacb37c99
Jens Nikolaus <jensnikolaus@gmail.com>
2023-02-16 19:23:35 -0700

    add ScrollView

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,16 +7,28 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            GeometryReader { geo in
-                Image("Example")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: geo.size.width * 0.8)
-                    .frame(width: geo.size.width, height: geo.size.height)
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText(text: "Hi \($0)")
+                }
             }
+            .frame(maxWidth: .infinity)
         }
     }
 }
