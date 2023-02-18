a6109c65d9d8eafab0019ad46db48c8730302701
Jens Nikolaus <jensnikolaus@gmail.com>
2023-02-17 17:49:32 -0700

    refactor decode func to use generics

---
+++ Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Jens Nikolaus on 2/17/23.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
