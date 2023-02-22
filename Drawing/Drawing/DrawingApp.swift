eaf82b6ad69360cee37b8b92368b4febf2f629d4
Jens Nikolaus <jensnikolaus@gmail.com>
2023-02-22 15:07:06 -0700

    start new project Drawing on day 43

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Jens Nikolaus on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
