353c55c5e8bf1f15ee00b8824c727ce1a9cfef7f
Jens Nikolaus <jensnikolaus@gmail.com>
2023-03-01 15:18:07 -0700

    add arrow shape with var strokeWidth

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,55 +7,28 @@
 
 import SwiftUI
 
-struct Checkerboard: Shape {
-    var rows: Int
-    var columns: Int
-    
-    var animatableData: AnimatablePair<Double, Double> {
-        get {
-            AnimatablePair(Double(rows), Double(columns))
-        }
-        
-        set {
-            rows = Int(newValue.first)
-            columns = Int(newValue.second)
-        }
-    }
+struct Arrow: Shape {
     
     func path(in rect: CGRect) -> Path {
         var path = Path()
-        
-        let rowSize = rect.height / Double(rows)
-        let columnSize = rect.width / Double(columns)
-        
-        for row in 0..<rows {
-            for column in 0..<columns {
-                if (row + column).isMultiple(of: 2) {
-                    let startX = columnSize * Double(column)
-                    let startY = rowSize * Double(row)
-                    
-                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
-                    path.addRect(rect)
-                }
-            }
-        }
+        path.move(to: CGPoint(x: 200, y: 100))
+        path.addLine(to: CGPoint(x: 150, y: 200))
+        path.addLine(to: CGPoint(x: 250, y: 200))
+        path.addLine(to: CGPoint(x: 200, y: 100))
+        path.closeSubpath()
         
         return path
     }
 }
 
 struct ContentView: View {
-    @State private var rows = 4
-    @State private var columns = 4
+    @State private var strokeWidth = 10.0
     
     var body: some View {
-        Checkerboard(rows: rows, columns: columns)
-            .onTapGesture {
-                withAnimation(.linear(duration: 3)) {
-                    rows = 8
-                    columns = 8
-                }
-            }
+        VStack {
+            Arrow()
+                .stroke(.indigo, lineWidth: strokeWidth)
+        }
     }
 }
 
