52243727b3b5bd629baafd23489f0e4d053404c2
Jens Nikolaus <jensnikolaus@gmail.com>
2023-03-03 10:38:02 -0700

    add hints and Activity struct

---
+++ HabitTracking/HabitTracking/Activity.swift
@@ -0,0 +1,15 @@
+//
+//  Activity.swift
+//  HabitTracking
+//
+//  Created by Jens Nikolaus on 3/3/23.
+//
+
+import Foundation
+
+struct Activity: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+    let count: Int
+}
--- HabitTracking/HabitTracking/ContentView.swift
+++ HabitTracking/HabitTracking/ContentView.swift
@@ -4,6 +4,11 @@
 //
 //  Created by Jens Nikolaus on 3/3/23.
 //
+//  Define a struct that holds a single activity, and a class that holds an array of activities.
+//  The class will need to conform to ObservableObject and use @Published for its property.
+//  Your main listing and form should both be able to read the shared activities object.
+//  Make sure your activity conforms to Identifiable to avoid problems.
+//  Present your adding form using sheet(), and your activity detail view (if you add one) using NavigationLink.
 
 import SwiftUI
 
